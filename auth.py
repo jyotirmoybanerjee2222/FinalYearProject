7037b7eae8fcceffa1aa08b0eaeb79ab27b35507
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2025-06-07 23:04:35 +0530

    changes

--- auth.py
+++ auth.py
@@ -2,16 +2,7 @@ import hashlib
 import re
 import pyrebase
 
-firebaseConfig = {
-    "apiKey": "AIzaSyA98BWzuQfC2CdAmv7fWGK0PeHA3v2zLas",
-    "authDomain": "contexify-16827.firebaseapp.com",
-    "projectId": "contexify-16827",
-    "storageBucket": "contexify-16827.firebasestorage.app",
-    "messagingSenderId": "991703954388",
-    "appId": "1:991703954388:web:c7f9ae8e0bd231fac1dc41",
-    "measurementId": "G-PWVJ86R5N8",
-    "databaseURL": ""  # Optional unless you use Firebase Realtime DB
-}
+
 
 
 firebase = pyrebase.initialize_app(firebaseConfig)
